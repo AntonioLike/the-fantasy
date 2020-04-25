bb6df776c58bdfbe089a901fa0dff98cf87d13ec
antonio craveiro <pe_1992@hotmail.com>
2020-04-25 17:15:34 +0200

    Spring-boot project

---
+++ src/main/java/com/github/antoniolike/thefantasy/TheFantasyApplication.java
@@ -0,0 +1,13 @@
+package com.github.antoniolike.thefantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFantasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheFantasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/antoniolike/thefantasy/TheFantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.antoniolike.thefantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheFantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
